0789ce331bf33366f5c1f05a223fb7fbc3d338e3
7saru <saboo9653@gmail.com>
2023-03-21 06:47:06 +0200

    fixing share tooltip in mobile

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const shareBtn = document.querySelector(".share");
 const popup = document.querySelector(".popup-mobile");
 
 shareBtn.addEventListener("click", () => {
-  shareBtn.classList.toggle("active");
   popup.classList.toggle("active");
 });
